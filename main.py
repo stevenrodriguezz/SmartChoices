1789b6398e4de22b483fdfd8268d5c2d1b30d49c
Edrop7 <130793043+Edrop7@users.noreply.github.com>
2024-09-28 13:37:34 -0400

    debt + investments + interest methods created

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from utils import Player, start_game
 
-Player1 = Player("Mechanic", 60000)
-Player2 = Player("Nurse", 90000)
-Player3 = Player("Grocer", 15000)
+Player1 = Player("Mechanic", 60000, 550)
+Player2 = Player("Nurse", 90000, 630)
+Player3 = Player("Grocer", 15000, 470)
 
 players = [Player1, Player2, Player3]
 
 start_game(players)
 
-print(Player1.get_logged_in())
+Player1.investment_transaction(10000)
+Player1.investment_accrual()
+
+print(Player1.get_investment_account())
--- utils.py
+++ utils.py
@@ -23,15 +23,24 @@ prior_bracket_taxes = [(income_per_bracket[0] * tax_per_bracket[0]),
                     ((income_per_bracket[4] - income_per_bracket[3]) * tax_per_bracket[4]),
                     ((income_per_bracket[5] - income_per_bracket[4]) * tax_per_bracket[5])]
 
+interest = {
+    "credit": 1.3,
+    "investment": 1.1
+}
+
 class Player:
-    def __init__(self, job, salary):
+    def __init__(self, job, salary, credit_score):
+        self.set_logged_in(False)
         self.set_job(job)
         self.set_salary(salary)
         self.set_bank(0)
+        self.set_debt(0)
+        self.set_investment_account(0)
+        self.set_credit_score(credit_score)
         self.set_taxes(salary)
         self.set_monthly_income()
         self.set_monthly_taxes()
-        self.set_logged_in(False)
+        self.set_credit_limit()
 
     def get_logged_in(self):
         return self.__logged_in
@@ -56,6 +65,24 @@ class Player:
 
     def set_bank(self, money):
         self.__bank = round(money, 2)
+
+    def get_debt(self):
+        return self.__debt
+
+    def set_debt(self, debt):
+        self.__debt = round(debt, 2)
+
+    def get_investment_account(self):
+        return self.__investment_account
+
+    def set_investment_account(self, money):
+        self.__investment_account = round(money, 2)
+
+    def get_credit_score(self):
+        return self.__credit_score
+
+    def set_credit_score(self, credit_score):
+        self.__credit_score = credit_score
     
     def get_taxes(self):
         return self.__taxes
@@ -103,15 +130,50 @@ class Player:
     def set_monthly_taxes(self):
         self.__monthly_taxes = round(((self.get_taxes()) / 12), 2)
 
+    def get_credit_limit(self):
+        return self.__credit_limit
+
+    def set_credit_limit(self):
+        multiplier = 1
+        if self.get_credit_score() < 400:
+            multiplier = 0.25
+        elif self.get_credit_score() < 500:
+            multiplier = 0.5
+        elif self.get_credit_score() < 600:
+            multiplier = 0.75
+        elif self.get_credit_score() < 700: 
+            multiplier = 1
+        elif self.get_credit_score() < 800:
+            multiplier = 1.25
+        else:
+            multiplier = 1.5
+        self.__credit_limit = round(self.get_monthly_income() * multiplier - self.get_debt(), 2)
+
+
     job = property(get_job, set_job)
     salary = property(get_salary, set_salary)
     bank = property(get_bank, set_bank)
+    credit_score = property(get_credit_score, set_credit_score)
+    debt = property(get_debt, set_debt)
     taxes = property(get_taxes, set_taxes)
     monthly_income = property(get_monthly_income, set_monthly_income)
     monthly_taxes = property(get_monthly_taxes, set_monthly_taxes)
+    credit_limit = property(get_credit_limit, set_credit_limit)
 
     def bank_transaction(self, money):
         self.set_bank(self.get_bank() + money)
+
+    def debt_transaction(self, debt):
+        self.set_debt(self.get_debt() + debt)
+
+    def debt_accrual(self):
+        self.set_debt(self.get_debt() * interest["credit"])
+
+    def investment_transaction(self, money):
+        self.set_investment_account(self.get_investment_account() + money)
+
+    def investment_accrual(self):
+        self.set_investment_account(self.get_investment_account() * interest["investment"])
 
     def round_start_routine(self):    
         self.bank_transaction(self.get_monthly_income())
@@ -122,4 +184,6 @@ def start_game(players):
         player.round_start_routine()
         print(f"""Your job is {player.get_job()} \n your salary is {player.get_salary()} so you make {round(player.get_salary() / 12, 2)} per month \n
                 you pay {round(player.get_monthly_taxes(), 2)} in taxes, thus your income is {player.get_monthly_income()} per month \n
-                you have {player.get_bank()} in bank \n \n""")
+                you have {player.get_bank()} in bank \n your credit score is {player.get_credit_score()} \n 
+                you have {player.get_debt()} debt \n your credit limit is {player.get_credit_limit()} \n
+                you have {player.get_investment_account()} money invested \n \n""")
